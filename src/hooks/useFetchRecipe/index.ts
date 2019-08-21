1e5ad97a34501787171cf5cf10aa4e9d412825cc
robshape <robshape@users.noreply.github.com>
2019-08-21 19:53:41 +0200

    update recipes count

--- src/hooks/useFetchRecipe/index.ts
+++ src/hooks/useFetchRecipe/index.ts
@@ -5,7 +5,7 @@ import { randomNumber, randomNumberWithIncrement } from '../../common/util';
 import { Recipe } from '../../common/type';
 
 export const useFetchRecipe = (): FetchRecipe => {
-  const RECIPES_COUNT = 153; // Total number of recipes provided by API (2019-03-12).
+  const RECIPES_COUNT = 129; // Total number of recipes provided by API (2019-08-21).
   const RECIPES_INCREMENT = 20; // API serves recipes in increments of 20.
   const RECIPES_TAGS = '&tags[]=6479&tags[]=6480&tags[]=6495&tags[]=6500&tags[]=7020&tags[]=7023'; // Recipe categories to filter for: 6479=Billig, 6480=Enkel, 6495=Lättlagat, 6500=Snabb, 7020=Lunch, 7023=Middag.
 
